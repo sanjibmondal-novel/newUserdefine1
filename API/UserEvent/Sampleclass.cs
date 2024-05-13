3b05edb41edf24a67c34927c9b9980a1625a0e66
Sanjib Kumar Mondal <smondal@appsnovel.com>
2024-05-13 17:40:46 +0530

    Add files via upload

---
+++ API/UserEvent/Sampleclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newUserdefine1
+{
+    public  class Sampleclass
+    {
+        public virtual string GetName()
+        {
+            return "I am Test1";
+        }
+
+    }
+}
